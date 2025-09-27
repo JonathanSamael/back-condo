bece5d2a1b5a996e03dbaeeb0de1f9bd46a1cf2e
Jonathan Amaral <jonathansamael@live.com>
2025-09-27 18:12:38 -0300

    "feat: create swagger config"

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 const express = require('express')
-const prisma = require('./prismaClient')
-const app = express()
+const userRoutes = require("./routes/userRoutes")
+const swaggerUi = require('swagger-ui-express')
+const swaggerSpec = require("./config/swagger.config")
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
 app.use(express.json())
 
-app.get('/health', (req, res) => res.send('ok'))
+app.use("/api/users", userRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get("/hello", (req, res) => res.send('ok'));
 
-// rota de teste para listar users
-app.get('/users', async (req, res) => {
-  try {
-    const users = await prisma.user.findMany()
-    res.json(users)
-  } catch (err) {
-    res.status(500).json({ error: err.message })
-  }
-})
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('Servidor rodando na porta', process.env.PORT || 3000)
-})
+app.listen(port, () => {
+  console.log('🚀 Server running on http://localhost:3000')
+  console.log('📖 Swagger docs on http://localhost:3000/api-docs')
+});
